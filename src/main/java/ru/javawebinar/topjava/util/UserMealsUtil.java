c155c4d4fc8fb8d16c852c12fa096ad9116bd177
publicStaticEugene <eugenemikhnevich@gmail.com>
2018-07-04 22:32:10 +0300

    HW0 -> stream API implementation refactored

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -20,7 +20,7 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        List<UserMealWithExceed> mealsWithExceeded = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        final List<UserMealWithExceed> mealsWithExceeded = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         System.out.println(mealsWithExceeded);
     }
 
@@ -47,14 +47,10 @@ public class UserMealsUtil {
         return result;*/
 
         // implementation through the stream API
-        final Map<LocalDate, Integer> caloriesByDays = new HashMap<>();
-        LocalDate ld;
-        Integer calories;
-        for (final UserMeal meal : mealList) {
-            ld = meal.getDateTime().toLocalDate();
-            calories = meal.getCalories();
-            caloriesByDays.put(ld, caloriesByDays.getOrDefault(ld, 0) + calories);
-        }
+        Map<LocalDate, Integer> caloriesByDays = mealList.stream()
+                .collect(Collectors.groupingBy(
+                        meal -> meal.getDateTime().toLocalDate(),
+                        Collectors.summingInt(UserMeal::getCalories)));
 
         return mealList.stream()
                 .filter(meal -> TimeUtil.isBetween(meal.getDateTime().toLocalTime(), startTime, endTime))
